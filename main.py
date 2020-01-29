e16a57f16d30e1203050663a7ee6e9e6bf4171db
pavethran <56648287+pavethran@users.noreply.github.com>
2020-01-29 15:31:25 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+a = int(input("enter the value:"))
+b = int(input("enter the value2:" ))
+y = True
+n = False
+c = a+b
+print (c)
+while input('do you want countinue say (y/n):')!= 'n':
+    a = int(input("enter the value:"))
+    b = int(input("enter the value2:" ))
+    c = a+b
+    print (c)
+
+
+
+    
